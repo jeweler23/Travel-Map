8827f5f980b9e535cfd9d7a7666e83b86a7bda69
 jeweler23 <lopatin.lopatin2011@gmail.com>
2024-02-06 19:10:24 +0300

    feat:delete file

--- src/assets/consts/consts.js
+++
@@ -1,1 +0,0 @@
-export const API_KEY = 'f66e2585ad388e20c23c20566eefedc0'
--- src/assets/consts/index.js
+++
@@ -1,7 +0,0 @@
-const info = await fetch('https://restcountries.com/v3.1/all')
-const infoCountry = await info.json();
-
-export {infoCountry}
-
-// export infoCounty
-
