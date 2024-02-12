3efaf092de481fc82f0f8ad4f2e69a8e7bd94c88
 jeweler23 <lopatin.lopatin2011@gmail.com>
2024-02-12 20:59:22 +0300

    create pinia and router

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 import { createApp } from "vue";
+import { createPinia } from "pinia";
 import "./assets/style.css";
 import App from "./App.vue";
-import { createRouter } from 'vue-router'
+import { createRouter } from "vue-router";
 
 const app = createApp(App);
+const pinia = createPinia();
+const router = createRouter();
 
+app.use(pinia);
+app.use(router);
 app.mount("#app");
 // .component("fa", FontAwesomeIcon)
