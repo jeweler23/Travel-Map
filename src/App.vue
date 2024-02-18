c8468f867d8a093167298af7f16617977a49a2cb
jeweler23 <lopatin.lopatin2011@gmail.com>
2024-02-18 21:58:46 +0300

    feat: create home page

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,9 @@
 <template>
-  <Logo />
-  <Title />
-  <Country :country="infoCountries" :index="indexCountry" />
-  <Map
-    @showInfo="showInfo"
-    @getCoordsCountries="getIdCountries"
-    :capitalMarker="capitalCoords"
-    :country="infoCountries"
-    :index="indexCountry"
-  />
+  <RouterView />
 </template>
 
 <script setup>
-import Logo from "@/components/Logo.vue";
-import Map from "@/components/Map.vue";
-import Title from "@/components/Title.vue";
-import Country from "@/components/Country.vue";
-import { useCountriesStore } from "./store/CountriesStore";
-import { infoCountries } from "@/assets/consts/index.js";
-import { API_KEY } from "@/assets/consts/consts";
-import { ref } from "vue";
-
-
-const countriesStore = useCountriesStore()
-
-const show = ref(false);
-const indexCountry = ref(203);
-const capitalCoords = ref([55.7558, 37.6176]);
-
-const showInfo = (accept) => {
-  show.value = accept.value;
-};
-
-function getCoordsCapital(countries, index) {
-  return countries[index].capitalInfo.latlng;
-}
-
-async function getIdCountries(coord) {
-  await countriesStore.getInfoCountry(coord.lat, coord.lng);
-  const country = countriesStore.country
-  indexCountry.value = countriesStore.resultIdCountry(country._value, infoCountries);
-  capitalCoords.value = getCoordsCapital(infoCountries, indexCountry.value);
-}
+import { RouterView } from "vue-router";
 </script>
 
 <style scoped></style>
--- src/main.js
+++ src/main.js
@@ -1,16 +1,15 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import "./assets/style.css";
+
 import App from "./App.vue";
-import { createRouter, createWebHistory } from "vue-router";
+import router from "./router/router";
 
 const app = createApp(App);
 const pinia = createPinia();
-// const router = createRouter({
-//   history: createWebHistory(),
-// });
+
 
 app.use(pinia);
-// app.use(router);
+app.use(router);
 app.mount("#app");
 // .component("fa", FontAwesomeIcon)
